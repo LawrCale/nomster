ca438bff2e699ade4f42f624c7334f9009d165ba
Chris Calendine <ccalendine@yahoo.com>
2019-09-27 01:13:31 +0000

    edits java files

--- app/models/place.rb
+++ app/models/place.rb
@@ -3,5 +3,5 @@ class Place < ApplicationRecord
   has_many :comments
   geocoded_by :address
   after_validation :geocode
-  validates :name, presence: true
+  validates :name, presence: true, length: { minimum: 2 }
 end
